232f4551a09106fef56b66a5e315123d2ce9a088
ceo-py <sfmf@abv.bg>
2023-01-11 16:39:43 +0200

    workaround blizzard in correct api return for some char

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class PersistentViewBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.all()
 
-        super().__init__(command_prefix="?", help_command=None, intents=intents)
+        super().__init__(command_prefix="!", help_command=None, intents=intents)
 
     async def setup_hook(self) -> None:
         self.add_view(ButtonsCharacterStatistics())
