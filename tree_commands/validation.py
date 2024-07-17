4870d2bb3c71b2e4e964b8188daadd8d1d1a19e4
ceo-py <sfmf@abv.bg>
2024-07-17 11:12:15 +0300

    removing old version completely

--- tree_commands/validation.py
+++
@@ -1,8 +0,0 @@
-class ValidationTreeCommands:
-    @staticmethod
-    def hour(value):
-        return not 0 <= value <= 24
-
-    @staticmethod
-    def minute(value):
-        return not 0 <= value <= 59
