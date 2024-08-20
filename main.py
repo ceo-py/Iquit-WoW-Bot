499075fcf36ba903a71dfee88607a93c05cc6bd6
ceo-py <sfmf@abv.bg>
2024-08-20 16:13:37 +0300

    connect db with discord bot & move settings in main directory

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import datetime
 import discord
 from discord.ext import commands
-from settings import settings
+import settings
+from database.db import init_db
 
 SEASON = settings.WOW_CURRENT_EXPANSION
 EXPANSION = settings.WOW_CURRENT_SEASON
 BOT_TOKEN = settings.BOT_TOKEN
 
-UTC = datetime.timezone.utc
-times = [datetime.time(hour=x) for x in range(0, 24, 2)]
+# UTC = datetime.timezone.utc
+# times = [datetime.time(hour=x) for x in range(0, 24, 2)]
 
 
 class PersistentViewBot(commands.Bot):
@@ -20,6 +21,7 @@ class PersistentViewBot(commands.Bot):
     async def on_ready(self):
         # await client.tree.sync() # once only to sync CRUD slash command
         await self.change_presence(activity=discord.Game(name="M+"))
+        await init_db()
         # print([
         #     channel.id
         #     for server in self.guilds
