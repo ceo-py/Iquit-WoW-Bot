fea73615f92de08d984deb192ae5068fe03e6066
ceo-py <sfmf@abv.bg>
2024-08-22 18:36:21 +0300

    refactor remove msg

--- modals/remove_character_from_server_modal.py
+++ modals/remove_character_from_server_modal.py
@@ -14,23 +14,28 @@ class RemoveCharacterModal(BaseAddRemoveModal):
     def __init__(self, *args, **kwargs):
         super().__init__(title=self.TITLE, *args, **kwargs)
 
+    async def send_character_not_exist_message(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"Character does not exist in this server: {self.character_details_for_discord}.",
+            ephemeral=True,
+        )
+
+    async def send_success_message(self, interaction: discord.Interaction, character):
+        await interaction.response.send_message(
+            f"Character successfully removed from the server: {character_emojis.get(character.character_class)} {self.character_details_for_discord}.",
+        )
+
     async def on_submit(self, interaction: discord.Interaction) -> None:
         found_character_in_db = await self.found_character_in_db()
 
         if not found_character_in_db:
-            await interaction.response.send_message(
-                f"Character does not exist in this server: {self.character_details_for_discord}.",
-                ephemeral=True,
-            )
+            await self.send_character_not_exist_message(interaction)
             return
 
         server = await get_server_by_discord_id(interaction.channel_id)
 
         if not server:
-            await interaction.response.send_message(
-                f"Character does not exist in this server: {self.character_details_for_discord}.",
-                ephemeral=True,
-            )
+            await self.send_character_not_exist_message(interaction)
             return
 
         found_character_in_discord_server = await get_character_by_id_with_server_id(
@@ -38,13 +43,8 @@ class RemoveCharacterModal(BaseAddRemoveModal):
         )
 
         if found_character_in_discord_server is None:
-            await interaction.response.send_message(
-                f"Character does not exist in this server: {self.character_details_for_discord}.",
-                ephemeral=True,
-            )
+            await self.send_character_not_exist_message(interaction)
             return
 
         await delete_character_from_server(found_character_in_db.id, server.id)
-        await interaction.response.send_message(
-            f"Character successfully removed from the server: {character_emojis.get(found_character_in_db.character_class)} {self.character_details_for_discord}.",
-        )
+        await self.send_success_message(interaction, found_character_in_db)
