dec6f091b38f9ed573a4b1dc0f3d737a0e05f8fb
ceo-py <sfmf@abv.bg>
2024-09-16 13:44:01 +0300

    added base url for api rio fetching

--- utils/chunked_iterable.py
+++ utils/chunked_iterable.py
@@ -1,7 +1,7 @@
 import itertools
 
 
-def chunked_iterable(iterable: list, size) -> itertools.islice:
+def chunked_iterable(iterable: list, size: int) -> itertools.islice:
     it = iter(iterable)
     while True:
         chunk = tuple(itertools.islice(it, size))
--- utils/generate_api_url_for_char_fetch.py
+++ utils/generate_api_url_for_char_fetch.py
@@ -9,9 +9,15 @@ from settings import (
 )
 
 
+def base_url(character: dict):
+    return f'{WOW_API_URL}/characters/profile?region={character["region"]}&realm={character["realm"]}&name={character["name"]}&fields='
+
+
 def generate_api_url_for_char_fetch(character: dict) -> str:
-    return f'{WOW_API_URL}/characters/profile?region={character["region"]}&realm={character["realm"]}&name={character["name"]}&fields={ALTERNATIVE_RUN_FIELDS},{BEST_RUN_FIELDS},{CURRENT_SEASON_SCORE}'
+    base_url = base_url(character)
+    return f'{base_url}{ALTERNATIVE_RUN_FIELDS},{BEST_RUN_FIELDS},{CURRENT_SEASON_SCORE}'
 
 
 def generate_api_url_for_char_fetch_check(character: dict) -> str:
-    return f'{WOW_API_URL}/characters/profile?region={character["region"]}&realm={character["realm"]}&name={character["name"]}&fields={CURRENT_SEASON_SCORE},{RAID_PROGRESSION},{GEAR},{MYTHIC_PLUS_RECENT_RUNS}'
+    base_url = base_url(character)
+    return f'{base_url}{CURRENT_SEASON_SCORE},{RAID_PROGRESSION},{GEAR},{MYTHIC_PLUS_RECENT_RUNS}'
