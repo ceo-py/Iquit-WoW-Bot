24b7b8b0569fe68fe30d8a5276200c32b751aab2
ceo-py <sfmf@abv.bg>
2024-10-08 17:45:09 +0300

    added target rating on input modal for rating planner

--- modals/rating_planner_character_modal.py
+++ modals/rating_planner_character_modal.py
@@ -1,15 +1,6 @@
 import discord
 from .base_modal_add_remove_character import BaseAddRemoveModal
-from database.models.character import Character
 from utils.api.request_character_information import get_wow_character
-from database.service.server_service import get_server_by_discord_id, create_server
-from database.service.character_server_service import (
-    get_character_by_id_with_server_id,
-    create_character_server,
-)
-from database.service.character_service import create_character
-from database.service.dungeon_run_service import update_or_create_dungeon_run
-from database.service.dungeon_service import get_all_dungeons
 
 
 class RatingPlannerModal(BaseAddRemoveModal):
@@ -21,23 +12,46 @@ class RatingPlannerModal(BaseAddRemoveModal):
         self.max_key_level = discord.ui.TextInput(
             label="Max Key Level",
             placeholder="Dungeon key level (2-20)",
-            max_length=12,
+            max_length=2,
+        )
+        self.target_rating = discord.ui.TextInput(
+            label="Target Rating",
+            placeholder="Enter the Mythic+ rating you want to achieve",
+            max_length=2,
         )
 
         self.add_item(self.max_key_level)
+        self.add_item(self.target_rating)
 
     async def on_submit(self, interaction: discord.Interaction) -> None:
 
         await interaction.response.defer()
 
-        # character_fetch_data = await get_wow_character(
-        #     self.character_region_realm_name_dict
-        # )
+        character = await get_wow_character(
+            self.character_region_realm_name_dict
+        )
 
-        # character_details_for_message = f"{interaction.client.character_emojis.get(character_class.lower())} {self.character_details_for_discord(interaction)}"
-        # server = await get_server_by_discord_id(interaction.channel_id)
+        if character.get("statusCode") != 200 and not character.get("name"):
+            await self.send_character_not_exist_message_in_battle_net(interaction)
+            return
+        
+        if not self.max_key_level.value.isdigit() or self.max_key_level < 2 or self.max_key_level > 20:
+            await interaction.followup.send(
+                f"Please enter a valid key level between 2 and 20.",
+            )
+            return
+        
+        character_details_message = f"{interaction.client.character_emojis.get(character.get('class').lower())} {self.character_details_for_discord(interaction)}"
+        top_character_runs = character.get('mythic_plus_best_runs', [])
+        
+        if not top_character_runs:
+            await interaction.followup.send(
+                f"No Mythic+ runs found for {character_details_message}"
+            )
+            return
+        
 
-        # try:
-        #     await interaction.followup.send(message)
-        # except discord.errors.Forbidden as e:
-        #     print(f"AddCharacterModal:\n{e}")
+        try:
+            await interaction.followup.send(character_details_message)
+        except discord.errors.Forbidden as e:
+            print(f"AddCharacterModal:\n{e}")
